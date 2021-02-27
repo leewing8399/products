90ca24fccb68a1e8e58e4ea405439d64fc9d9fd3
Ying Buerger <leewing83@hotmail.com>
2021-02-27 11:48:45 +0100

    add products.py

---
+++ products.py
@@ -0,0 +1,13 @@
+products =[]
+
+while True:
+	name = input('请输入商品名称:')
+	if name == 'q':
+		break
+
+	price = input('价格:')
+	p = []
+	p.append(name)
+	p.append(price)
+	products.append(p)         # 省略写法为products.append([name,price])
+print(products)
